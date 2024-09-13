4c6da3c1b873fb551a9ec44439aeaac26be77fec
Miguel <gawerne60@icloud.com>
2024-09-13 10:10:26 -0600

    Changes

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,14 +1,40 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import Navbar from './components/Navbar';
+import AuthService from './utils/auth';  // Assuming this is your auth.js on the client side
+
+// Set up the HTTP connection to the GraphQL API
+const httpLink = createHttpLink({
+  uri: '/graphql',  // Replace this with your GraphQL endpoint
+});
+
+// Set up the context to include the authorization token
+const authLink = setContext((_, { headers }) => {
+  const token = AuthService.getToken();  // Retrieve token from localStorage
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',  // Attach token to the headers
+    },
+  };
+});
+
+// Initialize Apollo Client
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),  // Combine authLink and httpLink
+  cache: new InMemoryCache(),       // Set up caching
+});
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+    </ApolloProvider>
   );
 }
 
